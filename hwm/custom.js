e064e18290f322ece200905451906fae9c6f3cda
JackAndChat <107268019+JackAndChat@users.noreply.github.com>
2022-06-10 11:51:40 -0500

    Add files via upload

---
+++ hwm/custom.js
@@ -0,0 +1,11 @@
+/*!
+**|  CyTube Enhancements: Room Specific
+**|
+**@preserve
+*/
+if (!window[CHANNEL.name]) window[CHANNEL.name] = {};
+
+/* ####################  Room Fixes  #################### */
+
+
+/* End of Script */
