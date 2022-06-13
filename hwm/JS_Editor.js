a698af1d5a4d11c6f85ef5322ad7bf7daafadc86
JackAndChat <107268019+JackAndChat@users.noreply.github.com>
2022-06-13 09:41:08 -0500

    Add files via upload

---
+++ hwm/JS_Editor.js
@@ -0,0 +1,19 @@
+/*!
+**|  CyTube Customizations
+**|   
+**@preserve
+*/
+
+var ChannelName_Caption = 'MILFs';
+var Room_ID = 'hwm';
+
+var ALLOW_GUESTS = true;
+var LOAD_BOT = true;
+
+var ROOM_ANNOUNCEMENT = ""
+var MOD_ANNOUNCEMENT = "";
+
+// ##################################################################################################################################
+if (!this[CHANNEL.name]) this[CHANNEL.name] = {};
+
+$.getScript("https://cdn.statically.io/gh/jackAndChat/CyTube/main/loader.js");
