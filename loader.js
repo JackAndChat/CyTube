20737ce910bf0b3d0a937d05ca1e92526fc380af
JackAndChat <107268019+JackAndChat@users.noreply.github.com>
2022-06-13 09:27:41 -0500

    Add files via upload

--- loader.js
+++ loader.js
@@ -5,14 +5,13 @@
 */
 if (!window[CHANNEL.name]) window[CHANNEL.name] = {};
 
-//  Channel Settings->Edit->JavaScript: $.getScript("https://https://raw.githubusercontent.com/JackAndChat/CyTube/main//www/{debug.}loader.js");
-
 // Defaults
 var START = Date.now();
 if (typeof CUSTOM_LOADED === "undefined") var CUSTOM_LOADED = false;
 if (typeof ChannelName_Caption === "undefined") var ChannelName_Caption = CHANNELNAME;
 if (typeof Room_ID === "undefined") var Room_ID = "debug";
 if (typeof ALLOW_GUESTS === "undefined") var ALLOW_GUESTS = true;
+if (typeof MUTE_GUESTS === "undefined") var MUTE_GUESTS = false;
 if (typeof LOAD_BOT === "undefined") var LOAD_BOT = false;
 if (typeof BOT_NICK === "undefined") var BOT_NICK = "JackAndChatBot";
 if (typeof ROOM_ANNOUNCEMENT === "undefined") var ROOM_ANNOUNCEMENT = "";
@@ -21,7 +20,7 @@ if (typeof CHANNEL_DEBUG === "undefined") var CHANNEL_DEBUG = false;
 
 var IMABOT = (CLIENT.name.toLowerCase() == BOT_NICK.toLowerCase());
 
-let Base_URL = "https://cdn.jsdelivr.net/gh/jackandchat/cytube@main/";
+let Base_URL = "https://cdn.statically.io/gh/jackandchat/cytube/main/www/";
 let Room_URL = Base_URL + Room_ID + "/";
 let Logo_URL =  Room_URL + "logo.png";
 let Favicon_URL = Room_URL + "favicon.png";
@@ -30,6 +29,7 @@ let Favicon_URL = Room_URL + "favicon.png";
 
 const loadScript = function(filename){
   try {
+    filename += '.js';
     $.getScript(filename)
       .done(function(script, textStatus) {
         window.console.log("loader.Loading " + filename + ": " + textStatus );
@@ -52,19 +52,19 @@ const loadCSS = function(filename){
 if (!CUSTOM_LOADED) { // Load Once
   CUSTOM_LOADED = true;
   
-  loadScript(Base_URL + "common.js");
-  loadScript(Room_URL + "custom.js");
+  loadScript(Base_URL + "common");
+  loadScript(Room_URL + "custom");
   
   if (!ALLOW_GUESTS && (CLIENT.rank > 0)) {
-    loadScript(Base_URL + "noguests.js");
+    loadScript(Base_URL + "noguests");
   }
 
   if (IMABOT) {
-    // loadScript(Base_URL + "dbLocal.js");
-    loadScript(Base_URL + "roombot.js");
+    // loadScript(Base_URL + "dbLocal");
+    loadScript(Base_URL + "roombot");
   }
 
-  // loadScript(Base_URL + "betterpm.js");
+  // loadScript(Base_URL + "betterpm");
 
   $(document).ready(()=>{
     $(".navbar-brand").replaceWith('<span class="navbar-brand">' + ChannelName_Caption + "</span>");
